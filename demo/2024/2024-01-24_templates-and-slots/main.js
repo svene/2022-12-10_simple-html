f129f9426d700518d0163327c835d7253a0a2fc9
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-02-11 09:24:01 +0100

    code from original branch 'base/templates-and-slots' step 2: using template via webcomponent

--- demo/2024/2024-01-24_templates-and-slots/main.js
+++ demo/2024/2024-01-24_templates-and-slots/main.js
@@ -1,7 +1,18 @@
 function init() {
-    let template = document.getElementById("my-paragraph");
-    let templateContent = template.content;
-    document.body.appendChild(templateContent);
+    customElements.define(
+            "my-paragraph",
+            class extends HTMLElement {
+                constructor() {
+                    super();
+                    let template = document.getElementById("my-paragraph");
+                    let templateContent = template.content;
+
+                    const shadowRoot = this.attachShadow({ mode: "open" });
+                    shadowRoot.appendChild(templateContent.cloneNode(true));
+                }
+            },
+    );
+
 }
 
 init();
