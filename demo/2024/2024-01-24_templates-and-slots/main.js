71348f1d0511c6a61300609ac16093db81305b0d
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-02-11 09:22:49 +0100

    code from original branch 'base/templates-and-slots' step 1: define template and add it to dom via Javascript

---
+++ demo/2024/2024-01-24_templates-and-slots/main.js
@@ -0,0 +1,7 @@
+function init() {
+    let template = document.getElementById("my-paragraph");
+    let templateContent = template.content;
+    document.body.appendChild(templateContent);
+}
+
+init();
