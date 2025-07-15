e5c20d1c2139423ab38dd63f78749b0b19e5937d
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-07-15 08:22:01 +0200

    simple daisy-ui demo: light/dark theme

--- demo/2025/2025-07-15_daisyui/index.js
+++ demo/2025/2025-07-15_daisyui/index.js
@@ -1,3 +1,9 @@
 import * as tailwind from 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4.1.11/+esm';
 
 tailwind.startWatching();
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.setAttribute('data-theme', 'dark');
+} else {
+    document.body.setAttribute('data-theme', 'light');
+}
